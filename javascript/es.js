e4611a37d975ada124a3566bdf46d93d78cbfe36
Rodobrodo2 <rodondolo2@gmail.com>
2024-11-04 15:17:29 +0100

    Genera numeri pari da 0 a 20

--- javascript/es.js
+++ javascript/es.js
@@ -19,4 +19,8 @@ let outside = 2;
     let inside = 8;
     console.log(outside, inside);
 }
-console.log(outside, inside);
+// console.log(outside, inside); *inside avendo lo scope a blocco , all esterno del blocco non si stampa.
+
+for(i = 0; i <= 20; i +=2) {
+    console.log(i);
+}
