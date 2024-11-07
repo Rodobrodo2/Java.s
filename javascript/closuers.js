708b704561016c1a8425454180866c602ffd77e0
Rodobrodo2 <rodondolo2@gmail.com>
2024-11-07 15:58:56 +0100

    Persistenza delle variabili locali

--- javascript/closuers.js
+++ javascript/closuers.js
@@ -1,7 +1,24 @@
-function outerFunction(x) {
+/* function outerFunction(x) {
     function innerFunction(y) {
         return x + y
     }
     return innerFunction
 }
-console.log(outerFunction(5)(6));
+console.log(outerFunction(5)(6)); */
+
+function outerFunction(x, initalValue) {
+    let result = initalValue;
+    function innerFunction(y) {
+        result += y;
+        return result;
+    }
+    return innerFunction
+}
+
+const value = outerFunction(5, 10);
+console.log(value(6)); // 5 e il parametro x(che non usiamo ora) 10 e il parametro initalvalue che inizializza result.
+console.log(value(80)); // 80 u sopra 6 e il valore del parametro y o della funzione innerfunction.
+//ogni volta che chiamiamo value con un valore nuovo al parametro y, il result verra aggiornato con un nuovo valore:
+//(initalvalue)10 + 6(y)= 16 (result)
+// (initalvalue e il result salvato in precedenza):
+// 16 + 80(y) = 96.
