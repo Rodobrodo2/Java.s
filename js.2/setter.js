e2a5f96ff38129e77b68fa06584c88283945b084
Rodobrodo2 <rodondolo2@gmail.com>
2024-11-13 15:56:16 +0100

    Carico massimo

--- js.2/setter.js
+++ js.2/setter.js
@@ -35,3 +35,39 @@ console.log(`Chilometraggio aggiornato: ${auto.chilometraggio} km`);
 
 auto.chilometraggio = 3000;
 console.log(auto.mostraChilometraggio());
+
+class Camion extends Automobile {
+    caricoMassimo;
+    constructor(marca, modello, anno, chilometraggio = 0, caricoMassimo) {
+        super(marca, modello, anno, chilometraggio);
+        this.caricoMassimo = caricoMassimo;
+        this.caricoAttuale = 0;
+    }
+    
+    descrizione() {
+        return `${super.descrizione()} Può trasportare fino a ${this.caricoMassimo} kg.`;
+    }
+
+
+    carica(kg) {
+        if (this.caricoAttuale + kg <= this.caricoMassimo) {
+            this.caricoAttuale += kg;
+            console.log(`Carico aumentato di ${kg} kg. Carico attuale: ${this.caricoAttuale} kg.`);
+        } else {
+            console.warn("Impossibile caricare. Supera il carico massimo consentito!");
+        }
+    }
+
+    mostraCaricoAttuale() {
+        return `Il carico attuale è di ${this.caricoAttuale} kg su un massimo di ${this.caricoMassimo} kg.`;
+    }
+}
+
+let camion = new Camion("Mercedes", "Actros", 2023, 5000, 20000);
+console.log(camion.descrizione());
+
+camion.carica(5000);
+console.log(camion.mostraCaricoAttuale()); 
+
+camion.carica(18000);
+console.log(camion.mostraCaricoAttuale()); 
