73a4132d512c8d0b720765e9fb0fc4dab9017f8d
Rodobrodo2 <rodondolo2@gmail.com>
2024-11-18 17:40:35 +0100

    Estrazioni elementi base" a "Destructuring con nomi di variabili diverse

---
+++ featuresJs/es.js
@@ -0,0 +1,32 @@
+//Estrazione elementi base (node file.js)
+let numeri = [51, 52, 53, 54, 55];
+let [primo, secondo] = numeri;
+console.log(primo, secondo);
+
+//Saltare elementi durante il destructuring
+[primo, , terzo] = numeri;
+console.log(primo, terzo);
+
+//Destructuring con valori predefiniti (arr)
+let frutti = ["banane", "papaya", "mango", "ananas"];
+let [india, brasile, messico, costaRica, cina = "castagne"] = frutti;
+console.log("L'India e il paese che produce piu " + india);
+console.log("Il Brasile il paese che produce piu " + brasile);
+console.log("Il Messico e il paese che produce piu " + messico);
+console.log("Il Costa Rica e il paese che produce piu " + costaRica);
+console.log("La Cina e il paese che produce piu " + cina);
+
+//Estrazione di proprietà base 
+let utente = {
+    name: "Claudio",
+    zipCode: 34567
+}
+let {name, zipCode, city = "Torino",  university = "Informatica"} = utente;
+console.log(`Nome: ${name}`)
+console.log(`Zipcode: ${zipCode}`)
+
+//Destructuring con nomi di variabili diversi predefiniti(obj)
+console.log("Citta: " + city);
+console.log("Università: " + university);
+
+
