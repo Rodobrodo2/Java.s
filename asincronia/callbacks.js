87195642c5f9bfbaf8cd465f53b8c34433c2837a
Rodobrodo2 <rodondolo2@gmail.com>
2024-11-25 16:28:20 +0100

    Funzione con callback e passaggio di parametri/Callback annidai

--- asincronia/callbacks.js
+++ asincronia/callbacks.js
@@ -1,7 +1,9 @@
+// Funzioni di base con callback
 function eseguiOperazione(a, b, callback) {
     const result = a + b;
+    console.log(`Risultato della somma: ${result}`);
 
-    if (callback && typeof callback === "function") {
+    if (callback && typeof callback === 'function') {
         callback(result);
     }
 }
@@ -10,4 +12,41 @@ function stampaMessaggio(result) {
     console.log(`Il risultato ${result} è stato elaborato con successo`)
 }
 
-eseguiOperazione(2, 3, stampaMessaggio);
+eseguiOperazione(5, 3, stampaMessaggio);
+
+// Funzione con callback e passaggio di parametri
+function operazionePrincipale(callback) {
+    let numero = 5;
+    let result = numero * numero;
+    
+    if(callback && typeof callback === 'function') {
+        callback(result);
+    }
+}
+
+function gestisciRisultato(result) {
+    console.log(`Il callback ha ricevuto il risultato: ${result}`);
+}
+
+operazionePrincipale(gestisciRisultato);
+
+// Callback annidati
+function eseguiSomma(a, b, callback) {
+    const risultatoSomma = a + b;
+    console.log(`Risultato della somma: ${risultatoSomma}`);
+    
+    callback(risultatoSomma);
+}
+
+function eseguiMoltiplicazione(x, callback) {
+    const risultatoMoltiplicazione = x * 2;
+    console.log(`Risultato della moltiplicazione: ${risultatoMoltiplicazione}`);
+
+    callback(risultatoMoltiplicazione);
+}
+
+eseguiSomma(3, 5, function(risultatoSomma) {
+    eseguiMoltiplicazione(risultatoSomma, function(risultatoMoltiplicazione) {
+        console.log(`Il risultato finale è: ${risultatoMoltiplicazione}`);
+    });
+});
