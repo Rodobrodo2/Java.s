49c158cb56a8e8b0204171f350b8eed4cfd20928
Rodobrodo2 <rodondolo2@gmail.com>
2024-11-27 15:35:02 +0100

    Funzioni asincrone in serie

---
+++ asincronia/async.js
@@ -0,0 +1,21 @@
+// Funzioni asincrone in serie 
+async function firstFunction() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return "Risultato della prima funzione (2 secondi)";
+}
+
+async function secondFuntion() {
+    await new Promise((resolve) => setTimeout(resolve,3000));
+    return "Risultato della seconda funzione (3 secondi)";
+}
+
+async function callFunctions() {
+    console.log("Inizio chiamate in serie");
+    const firstResult = await firstFunction();
+    console.log(firstResult);
+
+    const secondResult = await secondFuntion();
+    console.loh(secondResult);
+}
+
+callFunctions();
